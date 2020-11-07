ef48d40387398c8e6838416fcf9cf4330b2c3153
lucassherwin <lksherwin@gmail.com>
2020-11-07 18:25:21 -0500

    changes to login form

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import Login from './components/Login.js';
 import Homepage from './components/Homepage.js';
 import Navbar from './components/Navbar.js';
 import Account from './components/Account.js';
+import Search from './components/Search.js';
 
 export class App extends Component {
   state = {
-    user: null
+    username: '',
+    search: ''
+  }
+
+  handleSearch = (event) => {
+    console.log(event.target.value);
+    let { name, value } = event.target;
+    this.setState({[name]: value});
+  }
+
+  handleLoginChange = (event) => {
+    console.log(event.target.value);
+    this.setState({[event.target.name]: event.target.value});
   }
 
   render() {
     return (
       <div>
         <Navbar />
+        <Search handleSearch={this.handleSearch} search={this.state.search} />
         <Switch>
           <Route exact path = '/'>
             <Homepage />
           </Route>
           <Route exact path='/login'>
-            <Login />
+            <Login user={this.state.username} handleLoginChange={this.handleLoginChange} />
           </Route>
           <Route exact path='/account'>
             <Account />
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function Login() {
+export default function Login(props) {
     return (
         <div>
             <h1>Login Page</h1>
+            <form>
+                <label>Username:</label>
+                <input type='text' name='username' value={props.username} onChange={props.handleLoginChange} />
+            </form>
         </div>
     )
 }
